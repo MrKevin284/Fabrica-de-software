8a3a949763baa25d44649b0026b9c3b92628de5f
Fernando Nonato <105546921+Cyberfn@users.noreply.github.com>
2024-02-15 13:10:25 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "0123";
 $dbname = "techdrive";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
